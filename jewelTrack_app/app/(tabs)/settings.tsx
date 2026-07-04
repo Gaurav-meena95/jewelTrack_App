23dadb8f6bdde2fb1f50b43c34e4726b3fc56637
Gaurav Meena <meenajigaurav9589@gmail.com>
2026-07-04 09:28:12 +0530

    chnges

---
+++ jewelTrack_app/app/(tabs)/settings.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import SettingsComponent from '../../components/Shopkeeper/Settings/settings';
+
+export default function SettingsPage() {
+  return <SettingsComponent />;
+}
